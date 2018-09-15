292fe52287a6e20cd26074f4187e70fd89827c64
Tony.Chen <Tony.Chen@localhost>
2018-09-15 11:20:14 -0400

    Initial Commit

---
+++ No One Eats Alone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  No One Eats Alone
+//
+//  Created by Tony.Chen on 9/15/18.
+//  Copyright © 2018 TC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
